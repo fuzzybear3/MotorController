575e302af6ecd9688c094c341399e3446ba48427
GUIDO <stevenguido.guido@gmail.com>
2018-08-10 00:07:10 -0700

    first

---
+++ MotorController/MotorController.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MotorController.ino
+ Created:	8/10/2018 12:01:06 AM
+ Author:	steve
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
